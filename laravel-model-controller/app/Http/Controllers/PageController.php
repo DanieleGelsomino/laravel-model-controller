e2e6313be1787ffc8d5c0a932ba4d6b18274eaa5
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-20 16:15:37 +0200

    problems with connections db

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Movie;
 class PageController extends Controller
 {
     public function index(){
+        $movies = Movie::all();
         return view('home');
     }
 }
