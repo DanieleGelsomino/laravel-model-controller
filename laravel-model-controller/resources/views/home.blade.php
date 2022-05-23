7c62b073d4c482e97ddbb97f05b1a484289f9900
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-23 11:40:03 +0200

    corrections with forelse

--- laravel-model-controller/resources/views/home.blade.php
+++ laravel-model-controller/resources/views/home.blade.php
@@ -4,19 +4,21 @@
     <div class="contaner-fluid px-5 p-5">
         <div class="row flex-wrap">
             <h1 class="text-center">Best Movies</h1>
-            @foreach ($movies as $movie)
+            @forelse ($movies as $movie)
                 <div class="col-2 m-3 pt-4 text-center dg-card">
-                    <h2 class="title">{{ $movie['title'] }}</h2>
-                    <img src="{{ $movie['poster'] }}" alt="{{ $movie['title'] }}">
+                    <h2 class="title">{{ $movie->title }}</h2>
+                    <img src="{{ $movie->poster }}" alt="{{ $movie->title }}">
                     <div class="mt-3">
                         <h5>Original Title: </h5>
-                        <h3>{{ $movie['original_title'] }}</h3>
+                        <h3>{{ $movie->original_title }}</h3>
                     </div>
-                    <h5>Nationality: {{ $movie['nationality'] }}</h5>
-                    <h5>Release Date: {{ $movie['date'] }}</h5>
-                    <h5>Vote: {{ $movie['vote'] }}/10</h5>
+                    <h5>Nationality: {{ $movie->nationality }}</h5>
+                    <h5>Release Date: {{ $movie->date }}</h5>
+                    <h5>Vote: {{ $movie->vote }}</h5>
                 </div>
-            @endforeach
+            @empty
+                <h3>Non sono presenti film!</h3>
+            @endforelse
         </div>
     </div>
 @endsection
